aa8596610c24a5d2c5a1565466fd1a31e6764dc9
VIKNESWARAN GAUNDER <VC23028@adab.umpsa.edu.my>
2026-01-23 19:19:39 +0800

    Update maintenance task alerts and calendar

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/SFEMS_Frontend/',
-  build: {
-    chunkSizeWarningLimit: 1000,
-    copyPublicDir: true
-  }
 })
